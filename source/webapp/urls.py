9938c75e590b38dd2d885c4ce41a6d3713c5ecc8
nargiza-jumagulova <impression1108@gmail.com>
2022-01-08 03:40:24 +0600

    создали просмотр одного товара

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, product_view
 
 urlpatterns = [
-     path('', index_view)
+     path('', index_view, name="index"),
+     path('product/<int:pk>/', product_view, name="product_view")
+
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -7,3 +7,9 @@ from webapp.models import Product
 def index_view(request):
     products = Product.objects.filter(remainder__gte=1).order_by('category', 'name')
     return render(request, "index.html", {'products': products})
+
+
+def product_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, "product_view.html", {'product': product})
+
