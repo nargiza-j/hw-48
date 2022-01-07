82d3e9714fb1720efee59d6fe3f8961122b5e010
nargiza-jumagulova <impression1108@gmail.com>
2022-01-07 22:57:49 +0600

    создана модель Product, сделана и применена ее миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = [('0', 'other'), ('1', 'smartphone'),  ('2', 'computers')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Product name')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Product description')
+    category = models.CharField(max_length=30, default='0', null=False, blank=False, choices=CATEGORY_CHOICES, verbose_name='Category')
+    remainder = models.PositiveIntegerField(verbose_name='Remainder')
+    price = models.DecimalField(verbose_name='Price', max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.category} - {self.price}"
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
