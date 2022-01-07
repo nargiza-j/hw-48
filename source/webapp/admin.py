81270f6213266fcbc24a33b76fd49e19e07e24a2
nargiza-jumagulova <impression1108@gmail.com>
2022-01-08 02:11:08 +0600

    создали superuser и зарегистрировали модель Product в панели администратора

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'price', 'remainder']
+    list_filter = ['category']
+    search_fields = ['name', 'description']
+    fields = ['name', 'description', 'category', 'price', 'remainder']
+
+
+admin.site.register(Product, ProductAdmin)
