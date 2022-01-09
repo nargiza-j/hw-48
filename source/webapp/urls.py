9dfc6874e3e32b2d23ecbdddfadf31f6b0649895
nargiza-jumagulova <impression1108@gmail.com>
2022-01-09 23:58:07 +0600

    создали функцию create_product_view для добавления нового товара, зарегистрировали ее в url, создали шаблон и подключили ссылку на главной странице

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view, product_view
+from webapp.views import index_view, product_view, create_product_view
 
 urlpatterns = [
      path('', index_view, name="index"),
-     path('product/<int:pk>/', product_view, name="product_view")
-
+     path('product/<int:pk>/', product_view, name="product_view"),
+     path('product/add/', create_product_view, name='product_add')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from webapp.forms import ProductForm
 from webapp.models import Product
 
 
@@ -12,4 +13,16 @@ def index_view(request):
 def product_view(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, "product_view.html", {'product': product})
+
+
+def create_product_view(request):
+    if request.method == 'GET':
+        form = ProductForm()
+        return render(request, 'product_create.html', {"form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            new_product = form.save()
+            return redirect('product_view', pk=new_product.pk)
+        return render(request, 'product_create.html', {"form": form})
 
