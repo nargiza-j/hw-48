ce60265166a3201af8447151af22ab4a9c53c150
nargiza-jumagulova <impression1108@gmail.com>
2022-01-09 23:38:22 +0600

    создали форму и ее шаблон

---
+++ source/webapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import widgets
+
+from webapp.models import Product
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
