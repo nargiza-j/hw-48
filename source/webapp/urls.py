729be8952e3bcb718b3ceaa4d59a5dfde878c525
nargiza-jumagulova <impression1108@gmail.com>
2022-01-10 00:26:52 +0600

    создали функции для редактирования и удаления товара, зарегистрировали их в url, создали шаблоны и подключили ссылки на главной странице, а также на странице просмотра 1 товара

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from webapp.views import index_view, product_view, create_product_view
+from webapp.views import index_view, product_view, create_product_view, product_delete_view, product_update_view
 
 urlpatterns = [
      path('', index_view, name="index"),
      path('product/<int:pk>/', product_view, name="product_view"),
-     path('product/add/', create_product_view, name='product_add')
+     path('product/add/', create_product_view, name='product_add'),
+     path('product/<int:pk>/delete/', product_delete_view, name='product_delete'),
+     path('product/<int:pk>/update/', product_update_view, name='product_update')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from webapp.forms import ProductForm
@@ -26,3 +26,30 @@ def create_product_view(request):
             return redirect('product_view', pk=new_product.pk)
         return render(request, 'product_create.html', {"form": form})
 
+
+def product_update_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        form = ProductForm(initial={
+            'name': product.name,
+            'description': product.description,
+            'category': product.category,
+            'remainder': product.remainder,
+            'price': product.price
+        })
+        return render(request, 'product_update.html', {"product": product, "form": form})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_view', pk=product.pk)
+        return render(request, 'product_update.html', {"product": product, "form": form})
+
+
+def product_delete_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        return render(request, "product_delete.html", {"product": product})
+    else:
+        product.delete()
+        return redirect("index")
