5ad46cc29ebf0ae5c45e9edad8aa85822f627d41
nargiza-jumagulova <impression1108@gmail.com>
2022-02-05 00:56:52 +0600

    переписали редактирование продукта и удаление продукта на UpdateView и DeleteView соответственно

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from webapp.views.product_views import product_delete_view, \
-     product_update_view, ProductList, ProductView, ProductCreate
+from webapp.views.product_views import ProductList, ProductView, ProductCreate, ProductUpdate, ProductDelete
 
 urlpatterns = [
      path('', ProductList.as_view(), name="index"),
      path('product/<int:pk>/', ProductView.as_view(), name="product_view"),
      path('product/add/', ProductCreate.as_view(), name='product_add'),
-     path('product/<int:pk>/delete/', product_delete_view, name='product_delete'),
-     path('product/<int:pk>/update/', product_update_view, name='product_update')
+     path('product/<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
+     path('product/<int:pk>/update/', ProductUpdate.as_view(), name='product_update')
 ]
--- source/webapp/views/product_views.py
+++ source/webapp/views/product_views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
-
 # Create your views here.
-from django.views.generic import DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
 from webapp.forms import ProductForm
 from webapp.models import Product
@@ -31,46 +30,13 @@ class ProductCreate(CreateView):
     template_name = 'products/product_create.html'
 
 
-# def create_product_view(request):
-#     if request.method == 'GET':
-#         form = ProductForm()
-#         return render(request, 'products/product_create.html', {"form": form})
-#     else:
-#         form = ProductForm(data=request.POST)
-#         if form.is_valid():
-#             new_product = form.save()
-#             return redirect('product_view', pk=new_product.pk)
-#         return render(request, 'products/product_create.html', {"form": form})
+class ProductUpdate(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/product_update.html'
 
 
-def product_update_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'GET':
-        form = ProductForm(initial={
-            'name': product.name,
-            'description': product.description,
-            'category': product.category,
-            'remainder': product.remainder,
-            'price': product.price
-        })
-        return render(request, 'products/product_update.html', {"product": product, "form": form})
-    else:
-        form = ProductForm(data=request.POST)
-        if form.is_valid():
-            product.name = form.cleaned_data.get('name')
-            product.description = form.cleaned_data.get('description')
-            product.category = form.cleaned_data.get('category')
-            product.remainder = form.cleaned_data.get('remainder')
-            product.price = form.cleaned_data.get('price')
-            product.save()
-            return redirect('product_view', pk=product.pk)
-        return render(request, 'products/product_update.html', {"product": product, "form": form})
-
-
-def product_delete_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'GET':
-        return render(request, "products/product_delete.html", {"product": product})
-    else:
-        product.delete()
-        return redirect("index")
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = "products/product_delete.html"
+    success_url = reverse_lazy('index')
